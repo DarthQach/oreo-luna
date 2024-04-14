b9df4c53b1d8ee9bbfe368b9cf6d92cc9246f475
Carlos Villegas <darth.qach@gmail.com>
2024-04-14 08:12:07 -0600

    Change delay from inline to external file

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  setTimeout(function () {
+    document.getElementById("nextButton").disabled = false;
+  }, 1000); // 7000 milliseconds = 7 seconds
+});

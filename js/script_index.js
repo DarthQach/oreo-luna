fbd2c48e64b0fe76c8865b7975453999436eb1a9
Darth Qach <108153151+DarthQach@users.noreply.github.com>
2024-04-18 15:25:02 -0600

    Adding 10 sec to all pages and 20 sec to index

--- js/script_index.js
+++ js/script_index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   setTimeout(function () {
     document.getElementById("nextButton").disabled = false;
-  }, 10000); // 7000 milliseconds = 7 seconds
+  }, 20000); // 7000 milliseconds = 7 seconds
 });
